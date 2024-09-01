ab241ca6657cd6a11e8a09e680ed395de9465c79
VedanshiK <vedanshik100@gmail.com>
2024-09-01 23:26:09 +0530

    v commit

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -18,20 +18,25 @@ const Homepage = () => {
             and analysis of cyber incidents that impact Indian cyberspace.
           </h3>
         </div>
-      <div class="flex gap-4 pl-5 mt-4">
-      <Link to="/signup">
-        <button className="border border-richblack-700 bg-richblack-800 text-[12px] lg:text-[16px] px-2 py-1  lg:px-[12px] lg:py-[8px] bg-yellow-50 text-black font-semibold rounded-md hover:scale-95 transtion-all duration-200 ">
-          Sign Up
-        </button>
-      </Link>
+        <div class="flex gap-4 pl-5 mt-4">
+          <Link to="/signup">
+            <button className="border border-richblack-700 bg-richblack-800 text-[12px] lg:text-[16px] px-2 py-1  lg:px-[12px] lg:py-[8px] bg-yellow-50 text-black font-semibold rounded-md hover:scale-95 transtion-all duration-200 ">
+              Sign Up
+            </button>
+          </Link>
 
-        <Link to="/login">
-        <button className="border border-richblack-700 bg-richblack-700 text-[12px] lg:text-[16px] px-2 py-1  lg:px-[12px] lg:py-[8px] text-richblack-5 font-semibold rounded-md hover:scale-95 transtion-all duration-200 ">
-          Log In
-        </button>
-      </Link>
-      </div>
-       
+          <Link to="/login">
+            <button className="border border-richblack-700 bg-richblack-700 text-[12px] lg:text-[16px] px-2 py-1  lg:px-[12px] lg:py-[8px] text-richblack-5 font-semibold rounded-md hover:scale-95 transtion-all duration-200 ">
+              Log In
+            </button>
+          </Link>
+        </div>
+      </section>
+
+      {/* section2 */}
+      <section>
+        <p>What we do?</p>
+        
       </section>
     </div>
   );
